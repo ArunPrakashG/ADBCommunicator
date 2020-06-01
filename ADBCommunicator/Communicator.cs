a9eb69cd43ad6b4733f975c4f907815495c158b1
Arun <arun.prakash.456789@gmail.com>
2020-06-02 02:15:00 +0530

    More implementation

--- ADBCommunicator/Communicator.cs
+++ ADBCommunicator/Communicator.cs
@@ -1,22 +1,29 @@
 using ADBCommunicator.Exceptions;
 using SharpAdbClient;
+using SharpAdbClient.DeviceCommands;
+using SharpAdbClient.Exceptions;
 using Synergy.Logging;
 using Synergy.Logging.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Reflection;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ADBCommunicator {
 	internal class Communicator : IDisposable {
 		private static readonly ILogger Logger = new Logger(nameof(Communicator));
 		private static readonly Mutex ServerInstanceMutex;
-		private static readonly AdbServer Server = new AdbServer();
+		private static readonly SemaphoreSlim ProcessSync = new SemaphoreSlim(1, 1);
+		private static readonly AdbServer Server;
 
 		private readonly DeviceMonitor DeviceMonitor;
+		private readonly AdbClient Client;
 		private readonly List<DeviceData> ConnectedDevices = new List<DeviceData>();
+		internal int DevicesCount => ConnectedDevices.Count;
 
 		static Communicator() {
 			string _mutexName = Assembly.GetExecutingAssembly().GetName().Name + "-Mutex";
@@ -40,13 +47,15 @@ namespace ADBCommunicator {
 			}
 
 			ServerInstanceMutex.WaitOne();
+			Server = new AdbServer();
 		}
 
 		internal Communicator() {
 			DeviceMonitor = new DeviceMonitor(new AdbSocket(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort)));
+			Client = new AdbClient();
 		}
 
-		internal void StartNewDeviceMonitor() {
+		internal void InitDeviceMonitor() {
 			if (!DeviceMonitor.IsRunning) {
 				DeviceMonitor.Start();
 			}
@@ -56,42 +65,6 @@ namespace ADBCommunicator {
 			DeviceMonitor.DeviceChanged += OnDeviceChanged;
 		}
 
-		private void OnDeviceChanged(object? sender, DeviceDataEventArgs e) {
-
-		}
-
-		private void OnDeviceDisconnected(object? sender, DeviceDataEventArgs e) {
-			if (e == null || e.Device == null) {
-				return;
-			}
-
-			ConnectedDevices.RemoveAll(x => x.Serial.Equals(e.Device.Serial));
-		}
-
-		private void OnDeviceConnected(object? sender, DeviceDataEventArgs e) {
-			if (e == null || e.Device == null) {
-				return;
-			}
-
-			if (IsDuplicate(e.Device.Serial)) {
-				return;
-			}
-
-			ConnectedDevices.Add(e.Device);
-		}
-
-
-
-		private bool IsDuplicate(string? serial) {
-			for (int i = 0; i < ConnectedDevices.Count; i++) {
-				if (ConnectedDevices[i].Serial.Equals(serial)) {
-					return true;
-				}
-			}
-
-			return false;
-		}
-
 		internal static bool StartServer(string? adbPath = null) {
 			if (string.IsNullOrEmpty(adbPath) && SearchCurrentDirectoryForADB(out adbPath)) {
 				if (string.IsNullOrEmpty(adbPath)) {
@@ -99,8 +72,121 @@ namespace ADBCommunicator {
 				}
 			}
 
-			Server.StartServer(adbPath + "/" + Constants.ADB_EXE_NAME, true);
+			Logger.Trace("ADB files found at " + adbPath + " directory.");
+			Logger.Info($"{Constants.ADB_EXE_NAME} found at {adbPath + "/" + Constants.ADB_EXE_NAME}");
+			Console.WriteLine(Server.StartServer(adbPath + "/" + Constants.ADB_EXE_NAME, false));
 			return true;
+		}
+
+		internal void InitClient() => Client.Connect(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort));
+
+		internal IEnumerable<DeviceData> GetDevicesYield() {
+			foreach(DeviceData device in ConnectedDevices) {
+				if(device == null) {
+					continue;
+				}
+
+				yield return device;
+			}
+		}
+
+		internal List<DeviceData> GetDevices() => ConnectedDevices;
+
+		internal Dictionary<string, string> DisplayAllPackages(DeviceData? device) {
+			if (DevicesCount == 0) {
+				Logger.Warning("No devices found.");
+			}
+
+			if (device == null) {
+				return default;
+			}
+
+			return new PackageManager(Client, device).Packages;
+		}
+
+		internal bool InstallPackage(DeviceData? device, string apkPath) {
+			if (DevicesCount == 0) {
+				Logger.Warning("No devices found.");
+			}
+
+			if (device == null || string.IsNullOrEmpty(apkPath)) {
+				return false;
+			}
+
+			new PackageManager(Client, device).InstallPackage(apkPath, true);
+			return true;
+		}
+
+		internal bool UninstallPackage(DeviceData? device, string packageName) {
+			if (DevicesCount == 0) {
+				Logger.Warning("No devices found.");
+			}
+
+			if (device == null || string.IsNullOrEmpty(packageName)) {
+				return false;
+			}
+
+			new PackageManager(Client, device).UninstallPackage(packageName);
+			return true;
+		}
+
+		internal async Task<string?> ExecuteCommand(string command, DeviceData? device, CancellationToken cancellationToken) {
+			if (DevicesCount == 0) {
+				Logger.Warning("No devices found.");
+			}
+
+			if (string.IsNullOrEmpty(command) || device == null) {
+				return null;
+			}
+
+			ConsoleOutputReceiver recevier = new ConsoleOutputReceiver();
+
+			try {				
+				await Client.ExecuteRemoteCommandAsync(command, device, recevier, cancellationToken).ConfigureAwait(false);
+			}
+			catch (AdbException a) {
+				Logger.Error(a.AdbError);
+				return null;
+			}
+			
+			return recevier.ToString();
+		}
+
+		private void OnDeviceChanged(object? sender, DeviceDataEventArgs e) {
+			if (e == null || e.Device == null) {
+				return;
+			}
+
+			Logger.Info($"{e.Device.Serial} has changed!");
+			for (int i = 0; i < ConnectedDevices.Count; i++) {
+				if (ConnectedDevices[i].Serial.Equals(e.Device.Serial)) {
+					ConnectedDevices[i] = e.Device;
+					break;
+				}
+			}
+		}
+
+		private void OnDeviceDisconnected(object? sender, DeviceDataEventArgs e) {
+			if (e == null || e.Device == null) {
+				return;
+			}
+
+			Logger.Info($"{e.Device.Serial} has been disconnected!");
+			int removedCount = ConnectedDevices.RemoveAll(x => x.Serial.Equals(e.Device.Serial));
+			Logger.Info($"'{removedCount}' device(s) removed from connected device collection.");
+		}
+
+		private void OnDeviceConnected(object? sender, DeviceDataEventArgs e) {
+			if (e == null || e.Device == null) {
+				return;
+			}
+
+			Logger.Info($"{e.Device.Serial} has been connected!");
+			if (IsDuplicate(e.Device.Serial)) {
+				return;
+			}
+
+			ConnectedDevices.Add(e.Device);
 		}
 
 		private static bool SearchCurrentDirectoryForADB(out string? adbDirectoryPath) {
@@ -112,8 +198,9 @@ namespace ADBCommunicator {
 					continue;
 				}
 
-				if (dir.Equals(Constants.ADB_DIRECTORY_NAME, StringComparison.OrdinalIgnoreCase)) {
-					int fileExistCount = 1;
+				DirectoryInfo dirInfo = new DirectoryInfo(dir);
+				if (dirInfo.Name.Equals(Constants.ADB_DIRECTORY_NAME, StringComparison.OrdinalIgnoreCase)) {
+					int fileExistCount = 0;
 					foreach (string file in Directory.GetFiles(dir)) {
 						if (string.IsNullOrEmpty(file)) {
 							continue;
@@ -143,9 +230,33 @@ namespace ADBCommunicator {
 			return false;
 		}
 
+		private DeviceData GetDevice(string serial) {			
+			if (string.IsNullOrEmpty(serial) || DevicesCount == 0) {
+				return null;
+			}
+
+			return ConnectedDevices.FirstOrDefault(x => x.Serial.Equals(serial));
+		}
+
+		private bool IsDuplicate(string? serial) {
+			if (string.IsNullOrEmpty(serial)) {
+				return false;
+			}
+
+			for (int i = 0; i < ConnectedDevices.Count; i++) {
+				if (ConnectedDevices[i].Serial.Equals(serial)) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+
 		public void Dispose() {
 			ServerInstanceMutex.ReleaseMutex();
 			ServerInstanceMutex.Dispose();
+			Client.Disconnect(new DnsEndPoint(IPAddress.Loopback.ToString(), AdbClient.AdbServerPort));
+			DeviceMonitor.Dispose();			
 		}
 	}
 }
--- ADBCommunicator/Program.cs
+++ ADBCommunicator/Program.cs
@@ -1,11 +1,243 @@
+using SharpAdbClient;
 using Synergy.Logging;
 using Synergy.Logging.EventArgs;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ADBCommunicator {
 	internal class Program {
-		private static void Main(string[] args) {
+		private static readonly Communicator Communicator = new Communicator();
+		private static readonly CancellationTokenSource KeepAliveToken = new CancellationTokenSource();
+		private static int ExitCode = 0;
+		private static bool DisposeAlreadyCalled;
+
+		private static async Task<int> Main(string[] args) {
+			Synergy.Extensions.Helpers.CloseProcess(Constants.ADB_EXE_NAME);
 			Logger.LogMessageReceived += Logger_LogMessageReceived;
+			AppDomain.CurrentDomain.ProcessExit += OnEnvironmentExit;
+			Console.CancelKeyPress += OnForceQuitApp;
+			Communicator.StartServer();
+			Communicator.InitDeviceMonitor();
+			Communicator.InitClient();
+
+			if(Communicator.DevicesCount == 0) {
+				Out("No devices found. If the device is already connected, recheck if USB Debugging is enabled in Developer settings!");
+			}
+
+			return await KeepAlive().ConfigureAwait(false);
+		}
+
+		private static void OnForceQuitApp(object sender, ConsoleCancelEventArgs e) => Exit(-1);
+
+		private static void OnEnvironmentExit(object? sender, EventArgs e) => Exit(0);
+
+		private static async Task<int> KeepAlive() {
+			Out("press 'i' to install a package onto the device.");
+			Out("press 'u' to uninstall a package from the device.");
+			Out("press 'd' to display all connected devices on this pc.");
+			Out("press 'p' to display installed packages on the device.");
+			Out("press 'c' for interactive command processing.");
+
+			while (!KeepAliveToken.IsCancellationRequested) {
+				ConsoleKey key = Console.ReadKey(true).Key;
+				switch (key) {
+					case ConsoleKey.P:
+						if (Communicator.DevicesCount == 0) {
+							Out("No devices found.");
+						}
+
+						if (!DisplayAndGetSelection(out DeviceData? selectedDevice)) {
+							continue;
+						}
+
+						Dictionary<string, string> packages = Communicator.DisplayAllPackages(selectedDevice);
+
+						if(packages.Count > 0) {
+							foreach(var packPair in packages) {
+								Out($"{packPair.Key} | {packPair.Value}", ConsoleColor.Green);
+							}
+
+							continue;
+						}
+
+						Out("No packages can be found on the device.");
+						continue;
+					case ConsoleKey.I:
+						if (Communicator.DevicesCount == 0) {
+							Out("No devices found.");
+						}
+
+						if (!DisplayAndGetSelection(out selectedDevice)) {
+							continue;
+						}
+
+						Out("Enter the APK file path: ");
+						string apkPath = Console.ReadLine();
+
+						if (string.IsNullOrEmpty(apkPath)) {
+							Out("Invalid path.");
+							continue;
+						}
+
+						if (!File.Exists(apkPath)) {
+							Out("Such a file doesn't exist.");
+							continue;
+						}
+
+						Out("APK File path: " + apkPath, ConsoleColor.Green);
+						if (Communicator.InstallPackage(selectedDevice, apkPath)) {
+							Out("Operation completed successfully!");
+							continue;
+						}
+
+						Out("Operation failed.");
+						continue;
+					case ConsoleKey.U:
+						if (Communicator.DevicesCount == 0) {
+							Out("No devices found.");
+						}
+
+						if (!DisplayAndGetSelection(out selectedDevice)) {
+							continue;
+						}
+
+						Out("Enter the APK file path: ");
+						apkPath = Console.ReadLine();
+
+						if (string.IsNullOrEmpty(apkPath)) {
+							Out("Invalid path.");
+							continue;
+						}
+
+						if (!File.Exists(apkPath)) {
+							Out("Such a file doesn't exist.");
+							continue;
+						}
+
+						Out("APK File path: " + apkPath, ConsoleColor.Green);
+						if (Communicator.UninstallPackage(selectedDevice, apkPath)) {
+							Out("Operation completed successfully!");
+							continue;
+						}
+
+						Out("Operation failed.");
+						continue;
+					case ConsoleKey.D:
+						if (Communicator.DevicesCount == 0) {
+							Out("No devices found.");
+						}
+
+						DisplayDevices();
+						continue;
+					case ConsoleKey.C:
+						if (Communicator.DevicesCount == 0) {
+							Out("No devices found.");
+						}
+
+						Out("Starting interactive command processor...", ConsoleColor.Green);
+						if (!DisplayAndGetSelection(out selectedDevice)) {
+							continue;
+						}
+
+						const ConsoleKey exitKey = ConsoleKey.Q;
+						const int maxCommandWaitTime = 10;
+						Console.WriteLine(">> Press any key to Initiate command processor...");
+
+						while(Console.ReadKey(true).Key != exitKey) {
+							Console.Write("CMD >>> ");
+							string? cmd = Console.ReadLine();
+
+							if (string.IsNullOrEmpty(cmd)) {
+								Out($"Invalid command. Try again or press '{exitKey}' to exit the command processor.");
+								continue;
+							}
+
+							Out("Processing...", ConsoleColor.Green);
+							CancellationTokenSource commandToken = new CancellationTokenSource(TimeSpan.FromMinutes(maxCommandWaitTime));
+							string? result = await Communicator.ExecuteCommand(cmd, selectedDevice, commandToken.Token).ConfigureAwait(false);
+
+							if (string.IsNullOrEmpty(result)) {
+								Out("There is no result to display.");
+								continue;
+							}
+
+							Console.Write("RESULT >>> " + result);
+						}
+
+						continue;
+					default:
+						continue;
+				}
+			}
+
+			return ExitCode;
+		}
+
+		internal static void Exit(int exitCode = 0) {
+			if (DisposeAlreadyCalled) {
+				return;
+			}
+
+			Synergy.Extensions.Helpers.CloseProcess(Constants.ADB_EXE_NAME);
+			KeepAliveToken.Cancel();
+			DisposeAlreadyCalled = true;
+			Communicator.Dispose();			
+			ExitCode = exitCode;
+		}
+
+		private static void DisplayDevices() {
+			int count = 0;
+			foreach (DeviceData device in Communicator.GetDevicesYield()) {
+				Out($"{++count} | {device.Serial} | [{device.State}]");
+			}
+		}
+
+		private static bool DisplayAndGetSelection(out DeviceData? selectedDevice) {
+			int count = 0;
+
+			foreach (DeviceData device in Communicator.GetDevicesYield()) {
+				Out($"{++count} | {device.Serial} | [{device.State}]");
+			}
+
+			Out("Enter the Number of the device you want to execute commands on: ");
+			string sel = Console.ReadLine();
+
+			if (string.IsNullOrEmpty(sel) || !int.TryParse(sel, out int selectedIndex)) {
+				Out("Invalid selection; run again!");
+				selectedDevice = null;
+				return false;
+			}
+
+			try {
+				selectedDevice = Communicator.GetDevices().ElementAt(selectedIndex - 1);
+
+				if (selectedDevice == null) {
+					Out("No device selected.");
+					return false;
+				}
+
+				Out($"Selected device: {selectedDevice.Name}", ConsoleColor.Green);
+				return true;
+			}
+			catch {
+				Out("Such a device doesn't exist in the index.");
+				selectedDevice = null;
+				return false;
+			}
+		}
+
+		private static void Out(string? msg, ConsoleColor color = ConsoleColor.Gray) {
+			if (string.IsNullOrEmpty(msg)) {
+				return;
+			}
+
+			Console.ForegroundColor = color;
+			Console.WriteLine(">> " + msg);
+			Console.ResetColor();
 		}
 
 		private static void Logger_LogMessageReceived(object sender, LogMessageEventArgs e) {
